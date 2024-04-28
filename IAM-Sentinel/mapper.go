9cc16572085d5312bac23beed2ebf92157f58233
Richițeanu Sebastian <richiteanu.sebastian@gmail.com>
2024-04-28 16:41:22 +0300

    mapped base of groups, policies and roles

--- IAM-Sentinel/mapper.go
+++ IAM-Sentinel/mapper.go
@@ -22,10 +22,10 @@ func (m *ResourceMapper) MapFile(ctx context.Context, data []byte) error {
 	}
 
 	// TODO: This should be go func() but after it's stable enough :( or create var for PARALLEL?
+	m.mapGroups(ctx, gaad.GroupDetailList)
 	m.mapUsers(ctx, gaad.UserDetailList)
-	// m.mapGroups(gaad.GroupDetailList)
-	// m.mapRoles(gaad.RoleDetailList)
-	// m.mapPolicies(gaad.Policies)
+	m.mapRoles(ctx, gaad.RoleDetailList)
+	m.mapPolicies(ctx, gaad.Policies)
 
 	return nil
 }
@@ -36,13 +36,12 @@ func (m *ResourceMapper) mapUsers(ctx context.Context, users []UserDetail) {
 		// probably create them before and in create user just link :D
 
 		query := `
-		MERGE (user:IAMUser {key : $arn})
+		MERGE (user:User {key : $arn})
 			SET user.UserName = $userName
 			SET user.GroupList = $groups 
 			SET user.UserID = $userID
 			SET user.Path = $path
 			SET user.CreateDate = $createDate
-			
 		`
 		// SET user.ManagedPolicies = $managedPolicies
 		// SET user.PolicyList = $policyList
@@ -62,7 +61,107 @@ func (m *ResourceMapper) mapUsers(ctx context.Context, users []UserDetail) {
 			// change this to a logger
 			panic(err)
 		}
-
 	}
+}
 
+func (m *ResourceMapper) mapGroups(ctx context.Context, groups []GroupDetail) {
+	for _, group := range groups {
+		// TODO: idk if I should add policies here, probably just ->[:IS_PART_OF] or whatever
+		// probably create them before and in create user just link :D
+
+		query := `
+		MERGE (user:Group {key : $arn})
+			SET user.GroupID = $groupID
+			SET user.GroupName = $groupName 
+			SET user.Path = $path
+			SET user.CreateDate = $createDate
+		`
+		// SET user.ManagedPolicies = $managedPolicies
+		// SET user.PolicyList = $policyList
+
+		params := map[string]any{
+			"arn":        group.Arn,
+			"groupID":    group.GroupID,
+			"groupName":  group.GroupName,
+			"path":       group.Path,
+			"createDate": group.CreateDate,
+			//"managedPolicies": user.AttachedManagedPolicies,
+			//"policyList":      user.UserPolicyList,
+		}
+
+		if _, err := m.dbConn.ExecuteQueryWrite(ctx, query, params); err != nil {
+			// change this to a logger
+			panic(err)
+		}
+	}
+}
+
+func (m *ResourceMapper) mapPolicies(ctx context.Context, policies []Policy) {
+	for _, policy := range policies {
+		// TODO: idk if I should add policies here, probably just ->[:IS_PART_OF] or whatever
+		// probably create them before and in create user just link :D
+
+		query := `
+		MERGE (policy:Policy {key : $arn})
+			SET policy.PolicyID = $policyID
+			SET policy.PolicyName = $policyName 
+			SET policy.Path = $path
+			SET policy.CreateDate = $createDate
+			SET policy.AttachementCount = $attachementCount
+			SET policy.IsAttachable = $isAttachable
+			SET policy.DefaultVersionId = $defaultVersionID
+			SET policy.updatedDate = $updatedDate
+		`
+		// TODO: PolicyVersionList
+
+		params := map[string]any{
+			"arn":              policy.Arn,
+			"policyID":         policy.PolicyId,
+			"policyName":       policy.PolicyName,
+			"path":             policy.Path,
+			"createDate":       policy.CreateDate,
+			"attachementCount": policy.AttachmentCount,
+			"isAttachable":     policy.IsAttachable,
+			"defaultVersionID": policy.DefaultVersionId,
+			"updatedDate":      policy.UpdateDate,
+		}
+
+		if _, err := m.dbConn.ExecuteQueryWrite(ctx, query, params); err != nil {
+			// change this to a logger
+			panic(err)
+		}
+	}
+}
+
+func (m *ResourceMapper) mapRoles(ctx context.Context, roles []RoleDetail) {
+	for _, role := range roles {
+		// TODO: idk if I should add policies here, probably just ->[:IS_PART_OF] or whatever
+		// probably create them before and in create user just link :D
+
+		query := `
+		MERGE (role:Role {key : $arn})
+			SET role.RoleID = $roleID
+			SET role.RoleName = $roleName 
+			SET role.Path = $path
+			SET role.CreateDate = $createDate
+		`
+		// TODO: AssumeRolePolicyDocument
+		// InstanceProfileList
+		// AttachedManagedPolicies
+		// RoleLastUsed
+		// RolePolicyList
+
+		params := map[string]any{
+			"arn":        role.Arn,
+			"roleID":     role.RoleID,
+			"roleName":   role.RoleName,
+			"path":       role.Path,
+			"createDate": role.CreateDate,
+		}
+
+		if _, err := m.dbConn.ExecuteQueryWrite(ctx, query, params); err != nil {
+			// change this to a logger
+			panic(err)
+		}
+	}
 }
--- IAM-Sentinel/types.go
+++ IAM-Sentinel/types.go
@@ -4,13 +4,27 @@ import "time"
 
 type (
 	// get-account-authorization-details structure
+	// https://docs.aws.amazon.com/cli/latest/reference/iam/get-account-authorization-details.html
 	GAAD struct {
-		RoleDetailList  any          // Tighten to aws internal stuff but arrray???
-		GroupDetailList any          // same as above
-		UserDetailList  []UserDetail // same
-		Policies        any
+		RoleDetailList  []RoleDetail
+		GroupDetailList []GroupDetail
+		UserDetailList  []UserDetail
+		Policies        []Policy
 		Marker          string
 		IsTruncated     bool // if true, Marker is used to paginate
+	}
+
+	RoleDetail struct {
+		AssumeRolePolicyDocument any // TODO
+		RoleID                   string
+		CreateDate               time.Time
+		InstanceProfileList      any // TODO
+		RoleName                 string
+		Path                     string
+		AttachedManagedPolicies  []any // TODO
+		RoleLastUsed             any   // TODO
+		RolePolicyList           []any // TODO
+		Arn                      string
 	}
 
 	UserDetail struct {
@@ -22,5 +36,28 @@ type (
 		Path                    string   // ????
 		AttachedManagedPolicies []string // ???
 		Arn                     string
+	}
+
+	GroupDetail struct {
+		GroupID                 string
+		AttachedManagedPolicies any // TODO: tighten to policy
+		GroupName               string
+		Path                    string
+		Arn                     string
+		CreateDate              time.Time
+		GroupPolicyList         []any // TODO: tighten to policy
+	}
+
+	Policy struct { // maybe name PolicyDetail?
+		PolicyName        string
+		CreateDate        time.Time
+		AttachmentCount   int
+		IsAttachable      bool
+		PolicyId          string
+		DefaultVersionId  string // what? "v1"
+		PolicyVersionList any    // TODO
+		Path              string
+		Arn               string
+		UpdateDate        time.Time
 	}
 )
